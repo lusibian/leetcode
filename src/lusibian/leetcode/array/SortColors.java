fc937679c7314154ff5cc086f4b75742c8b5839b
lusibian <lusibian@xiaomi.com>
2020-01-26 12:36:12 +0800

    SortColors

--- src/lusibian/leetcode/array/SortColors.java
+++ src/lusibian/leetcode/array/SortColors.java
@@ -1,7 +1,8 @@
 package lusibian.leetcode.array;
 
 public class SortColors {
-    public void sortColors(int[] nums) {
+    // count
+    public void sortColors1(int[] nums) {
         int[] counts = new int[3];
         for (int num : nums) {
             counts[num]++;
@@ -14,6 +15,23 @@ public class SortColors {
         }
         for (int i = counts[0] + counts[1]; i < nums.length; i++){
             nums[i] = 2;
+        }
+    }
+
+    // swap
+    public void sortColors2(int[] nums) {
+        int zeroPointer = 0;
+        int twoPointer = nums.length - 1;
+        for (int i = 0; i <= twoPointer; i++) {
+            if(nums[i] == 0) {
+                nums[i] = nums[zeroPointer];
+                nums[zeroPointer] = 0;
+                zeroPointer++;
+            } else if(nums[i] == 2) {
+                nums[i] = nums[twoPointer];
+                nums[twoPointer] = 2;
+                twoPointer--;
+            }
         }
     }
 }
