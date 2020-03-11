7c883a02609848110d35fba4462739c64682e144
lusibian <lusibian@xiaomi.com>
2020-03-11 11:58:56 +0800

    完全背包问题

---
+++ src/lusibian/leetcode/dynamic_programming/CompletePack.java
@@ -0,0 +1,31 @@
+package lusibian.leetcode.dynamic_programming;
+
+import java.util.Scanner;
+
+public class CompletePack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numLine = sc.nextInt();
+        int packSize = sc.nextInt();
+        int[] weights = new int[numLine];
+        int[] values = new int[numLine];
+        for(int i = 0; i < numLine; i++){
+            weights[i] = sc.nextInt();
+            values[i] = sc.nextInt();
+        }
+        System.out.println(completerPack(weights, values, packSize));
+    }
+
+    // 完全背包
+    public static int completerPack(int[] weights, int[] values, int packSize) {
+        int[] pack = new int[packSize + 1];
+        for (int i = 0; i < weights.length; i++) {
+            for (int j = weights[i]; j <= packSize; j++) {
+                if (values[i] + pack[j - weights[i]] > pack[j]) {
+                    pack[j] = values[i] + pack[j - weights[i]];
+                }
+            }
+        }
+        return pack[packSize];
+    }
+}
