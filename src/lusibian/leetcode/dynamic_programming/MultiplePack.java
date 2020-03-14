7f6af31b811d44bb968fc008761d92422d374f24
lusibian <lusibian@xiaomi.com>
2020-03-14 19:52:05 +0800

    多重背包问题，添加单调队列优化，纠正错误解法

--- src/lusibian/leetcode/dynamic_programming/MultiplePack.java
+++ src/lusibian/leetcode/dynamic_programming/MultiplePack.java
@@ -1,6 +1,6 @@
 package lusibian.leetcode.dynamic_programming;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class MultiplePack {
     public static void main(String[] args) {
@@ -15,7 +15,10 @@ public class MultiplePack {
             values[i] = sc.nextInt();
             itemNums[i] = sc.nextInt();
         }
-        System.out.println(multiplePack3(weights, values, itemNums, packSize));
+//        System.out.println(multiplePack1(weights, values, itemNums, packSize));
+//        System.out.println(multiplePack2(weights, values, itemNums, packSize));
+//        System.out.println(multiplePack3(weights, values, itemNums, packSize));
+        System.out.println(multiplePack4(weights, values, itemNums, packSize));
     }
 
     // 多重背包
@@ -27,6 +30,13 @@ public class MultiplePack {
     // 若需要计算放满背包的方式数量，则新增辅助数组需记录当前item放入时的各种组合
     // 记item的总数为t，t为所有物品的数量之和
     // 时间复杂度O(mt)，空间复杂度O(mt/n)
+    //
+    // 算法有问题，辅助数组只记录当前item最少放入的数量，会导致错误的剪枝，应该保存所有中间结果才行
+    // 在输入为：
+    // 2 5
+    // 3 5 1
+    // 1 2 3
+    // 正确结果为9，算法输出7
     public static int multiplePack1(int[] weights, int[] values, int[] itemNums,int packSize) {
         int[] dp = new int[packSize + 1];
         int[] numInPackForCurrentItem = new int[packSize + 1];
@@ -101,6 +111,48 @@ public class MultiplePack {
                     }
                 }
             }
+        }
+        return dp[packSize];
+    }
+
+    // 多重背包
+    // 使用单调队列优化
+    // 优化的原理是啥呢？w[i]表示物品重量，v[i]表示价值，c[i]表示数量，我们知道朴素状态转移方程：
+    // f[i][j]=max(f[i−1][j−w[i]∗k]+v[i]∗k);(k<=c[i])
+    // 现在我们要把这个方程变成一个单调队列可以优化的形式，于是我们假设d=j mod w[i],s=⌊jw[i]⌋
+    // f[i][j]=max(f[i−1][d+w[i]∗k]−v[i]∗k)+v[i]∗s(s-k<=c[i])
+    // 所以我们枚举余数d，然后对于每个余数d都用单调队列优化即可。
+    //
+    // 记背包大小为m，物品种类为n
+    // 时间复杂度O(mn)，空间复杂度O(m)
+    public static int multiplePack4(int[] weights, int[] values, int[] itemNums,int packSize) {
+        int[] dp = new int[packSize + 1];
+        int[] tempDpArray = new int[packSize + 1];
+        Deque<Integer> idxQueue = new LinkedList<>();
+        for (int i = 0; i < weights.length; i++) {
+            for (int reminder = 0; reminder < weights[i]; reminder++) {
+                idxQueue.clear();
+                for (int j = 0; reminder + j * weights[i] <= packSize; j++) {
+                    if (!idxQueue.isEmpty() && j - idxQueue.getFirst() > itemNums[i]) {
+                        idxQueue.pop();
+                    }
+                    int valueForIdx = dp[reminder + weights[i] * j] - values[i] * j;
+                    while (!idxQueue.isEmpty()) {
+                        int last = idxQueue.getLast();
+                        int valueForLast =  dp[reminder + weights[i] * last] - values[i] * last;
+                        if (valueForLast < valueForIdx){
+                            idxQueue.removeLast();
+                        } else {
+                            break;
+                        }
+                    }
+                    idxQueue.add(j);
+                    int first = idxQueue.getFirst();
+                    int valueForFirst = dp[reminder + weights[i] * first] - values[i] * first;
+                    tempDpArray[reminder + j * weights[i]] = valueForFirst + values[i] * j;
+                }
+            }
+            System.arraycopy(tempDpArray, 0, dp, 0, packSize + 1);
         }
         return dp[packSize];
     }
