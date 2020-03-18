86d4aaf2620fc8247d09ec341c6cbc0a3974876e
lusibian <lusibian@xiaomi.com>
2020-03-18 22:01:56 +0800

    最长公共子序列，状态转移方程修正

--- src/lusibian/leetcode/dynamic_programming/_1143LongestCommonSubsequence.java
+++ src/lusibian/leetcode/dynamic_programming/_1143LongestCommonSubsequence.java
@@ -5,7 +5,7 @@ public class _1143LongestCommonSubsequence {
     // 子问题拆分：求字符串1的长度为i的前缀子串与字符串2的长度为j的前缀子串的最长公共子序列
     // 记c1[i]为字符串1的第i个字符，c2[j]为字符串2的第j个字符
     // 记h[i, j]为 c1[i]是否等于c2[j]，相等为1，不等为0
-    // 状态转移方程：dp[i, j] = max(dp[i-1, j], dp[i, j], dp[i-1, j-1] + h[i, j])
+    // 状态转移方程：dp[i, j] = max(dp[i-1, j], dp[i, j-1], dp[i-1, j-1] + h[i, j])
     // 时间复杂度O(mn)，空间复杂度O(m)或O(n)
     // 可以选择更短的那个字符串作为dp数组长度，空间复杂度O(min(m,n))
     public int longestCommonSubsequence(String text1, String text2) {
