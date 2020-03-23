649826f7bcc49ef54428dad8f7115a8572b1dc30
lusibian <lusibian@xiaomi.com>
2020-03-23 17:49:46 +0800

    188. Best Time to Buy and Sell Stock IV 状态机解法 时间开销优化

--- src/lusibian/leetcode/dynamic_programming/_188BestTimeToBuyAndSellStockIV.java
+++ src/lusibian/leetcode/dynamic_programming/_188BestTimeToBuyAndSellStockIV.java
@@ -8,14 +8,23 @@ public class _188BestTimeToBuyAndSellStockIV {
     //  i代表当前天数，k代表最大交易次数，0、1分别代表当前未持有或持有股票
     //
     // 记输入天数为n，最大交易次数为k
-    // 时间复杂度O(n*min(k, n/2))，空间复杂度O(min(k, n/2))
+    // 优化方法，当k大于等于n/2时，可以视为k为正无穷的情况，时间复杂度O(n)，空间复杂度O(1)
+    // 当k小于n/2时，时间复杂度O(nk)，空间复杂度O(k)
     public int maxProfit(int k, int[] prices) {
         if (prices.length == 0) {
             return 0;
         }
-        int maxTransactionTimes = Math.min(k, prices.length / 2);
-        int[][] dp = new int[maxTransactionTimes + 1][2];
-        int[][] currentDp = new int[maxTransactionTimes + 1][2];
+        if (k >= prices.length / 2) {
+            int dp_0 = 0, dp_1 = Integer.MIN_VALUE;
+            for (int price : prices) {
+                int temp = dp_0;
+                dp_0 = Math.max(dp_0, dp_1 + price);
+                dp_1 = Math.max(dp_1, temp - price);
+            }
+            return dp_0;
+        }
+        int[][] dp = new int[k + 1][2];
+        int[][] currentDp = new int[k + 1][2];
         for (int i = 0; i < dp.length; i++) {
             dp[i][1] = Integer.MIN_VALUE;
             currentDp[i][1] = Integer.MIN_VALUE;
@@ -29,6 +38,6 @@ public class _188BestTimeToBuyAndSellStockIV {
             dp = currentDp;
             currentDp = tempDp;
         }
-        return dp[maxTransactionTimes][0];
+        return dp[k][0];
     }
 }
