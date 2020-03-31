7c21420887759a7eda9df8575feaab7cd4beb0de
lusibian <lusibian@xiaomi.com>
2020-03-31 21:58:11 +0800

    28. Implement strStr()

---
+++ src/lusibian/leetcode/string/_28ImplementStrStr.java
@@ -0,0 +1,19 @@
+package lusibian.leetcode.string;
+
+public class _28ImplementStrStr {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            int j = 0;
+            while (j < needle.length() && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
